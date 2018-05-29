51fc513101c52a60f976bd6f480b9a9797c3e954
Philip Patton <ppatton35745@gmail.com>
2018-05-29 09:12:57 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NSS-Lightning-1
---
+++ main.js
@@ -0,0 +1,9 @@
+// 1. Open VS Code and open a new Quokka Javascript file.
+// 2. Create an array that holds at least five items. Each item should be a string that describes your experiance at NSS so far.
+// 3. Iterate over the array and console log each item in the array.
+
+const myExperience = ["Cool People", "Great Instructors", "Great Coffee", "Challenging But not overwhelmiong", "Excited to keep going"];
+
+for (let i=0; i < myExperience.length; i++) {
+    console.log(myExperience[i]);
+}
